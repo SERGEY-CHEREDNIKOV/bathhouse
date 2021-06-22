ce4b95a27466fd21432082727380e134f11c8732
Sergey <serezha.ros@mail.ru>
2021-06-22 07:31:49 +0300

    adding a tabs and slider-tab block

--- source/js/main.js
+++ source/js/main.js
@@ -59,10 +59,56 @@ window.addEventListener('load', function () {
 
     burgerMenu('.menu');
 
+
 // ---------- slider ------------    
+
 
     $('.about-us__slider').slick({
         prevArrow: '<button class="about-us__slider-btn  about-us__slider-btnprev"><img src="images/slider-btn-left.svg" alt="стрелка влево"></button>',
         nextArrow: '<button class="about-us__slider-btn  about-us__slider-btnnext"><img src="images/slider-btn-right.svg" alt="стрелка вправо"></button>',
     });
+
+    $('.slider-tab').slick({ 
+        slidesToShow: 3,
+        prevArrow: '<button class="slider-tab__slider-btn  slider-tab__slider-btnprev"><img src="images/slider-btn-left.svg" alt="стрелка влево"></button>',
+        nextArrow: '<button class="slider-tab__slider-btn  slider-tab__slider-btnnext"><img src="images/slider-btn-right.svg" alt="стрелка вправо"></button>',
+        responsive: [
+            {
+                breakpoint: 881,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 601,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ],
+    });
+ 
+    
+// ---------- tabs ------------   
+
+
+    const toSwitchTabs = (triggerBtn, content, activeBtn, activeContent) => {
+        const tabTrigger = document.querySelectorAll(triggerBtn);
+        const tabContent = document.querySelectorAll(content);
+
+        tabTrigger.forEach((item) => {
+            item.addEventListener('click', function (e) {
+                e.preventDefault();
+                const id = e.target.getAttribute('href').replace('#', '');
+
+                tabTrigger.forEach((child) => child.classList.remove(activeBtn));
+                tabContent.forEach((child) => child.classList.remove(activeContent));
+
+                item.classList.add(activeBtn);
+                document.getElementById(id).classList.add(activeContent);
+            });
+        });
+    };
+
+    toSwitchTabs('.tabs__triggers-item', '.tabs__content-item', 'tabs__triggers-item--active', 'tabs__content-item--active');
 });
